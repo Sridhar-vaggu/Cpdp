1e1a9c375c51e6b3f27a5841ef52243611ed42c7
Sridhar-vaggu <66628846+Sridhar-vaggu@users.noreply.github.com>
2023-04-28 21:40:44 -0400

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,23 +1,63 @@
+import queue
+import threading 
+
 N = 9
 
 count = 0
-
-def isSafe(sudoku, row, col, num):
+def checkRow(sudoku, row, num, result_queue):
     for i in range(9):
         if sudoku[row][i] == num:
-            return False
+            result_queue.put(False)
+            return
+        
+    result_queue.put(True)
 
+def checkCol(sudoku, col, num, result_queue):
     for i in range(9):
         if sudoku[i][col] == num:
-            return False
+            result_queue.put(False)
+            return
 
+    result_queue.put(True)
+
+def checkBlock(sudoku, row, col, num, result_queue):
     startRow = row - row % 3
     startCol = col - col % 3
     for i in range(3):
         for j in range(3):
             if sudoku[startRow + i][startCol + j] == num:
-                return False
-    return True
+                result_queue.put(False)
+                return
+
+    result_queue.put(True)
+
+def isSafe(sudoku, row, col, num):
+    result_queue = queue.Queue()
+
+    # Create threads to check the row, column, and block
+    row_thread = threading.Thread(target=checkRow, args=(sudoku, row, num, result_queue))
+    col_thread = threading.Thread(target=checkCol, args=(sudoku, col, num, result_queue))
+    block_thread = threading.Thread(target=checkBlock, args=(sudoku, row, col, num, result_queue))
+
+    # Start the threads
+    row_thread.start()
+    col_thread.start()
+    block_thread.start()
+
+    # Wait for the threads to finish
+    row_thread.join()
+    col_thread.join()
+    block_thread.join()
+
+    # Get the results from the queue
+    row_result = result_queue.get()
+    col_result = result_queue.get()
+    block_result = result_queue.get()
+
+    # If all three checks pass, return True
+     
+    return row_result and col_result and block_result
+
 
 def solveSudoku(sudoku, row, col):
     global count 
@@ -56,15 +96,4 @@ def solver(sudoku):
     else:
         count = 0
         return "no"
-    
-
-
-
-
-
-
-
-
-
-
     
